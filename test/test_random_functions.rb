23653035648f774f5d2f51f5e85c3b9b24f75e5f
plasmarobo@gmail.com <plasmarobo@gmail.com>
2014-03-28 23:30:18 -0400

    updated tests

--- test/test_random_functions.rb
+++ test/test_random_functions.rb
@@ -1,13 +1,47 @@
 require '../lib/random_functions.rb'
+
+require 'simpleoutput'
+require 'simplechartkick'
+require 'simpleplot'
+require 'simplelog'
+
 include RandomFunctions
 
-puts "Exponentials(30)"
-10.times {puts RandomFunctions.Exponential(30)}
-puts "Truncated Exp(30,60)"
-10.times {puts RandomFunctions.TruncatedExponential(30, 60)}
-puts "Uniform(0,5)"
-10.times {puts RandomFunctions.Uniform(0,5)}
+output = SimpleOutput::SimpleOutputEngine.new
+
+html = SimpleChartkick.new("GeneratorTest.html", "Generator test", '../include')
+plot = SimplePlot.new("_test")
+logger = SimpleLog.new("random_test")
+
+output.addPlugn(html)
+output.addPlugin(plot)
+output.addPlugin(logger)
+
+run = 10000
+exp = []
+run.times { exp << RandomFunctions.Exponential(30)}
+output.setArray(exp, "Exponential(30)", {"histogram" => true})
+exp = []
+run.times { exp << RandomFunctions.TruncatedExponential(30, 60)}
+output.setArray(exp, "Trunc Exp(30,60)", {"histogram" => true, 'ymin' => 0, 'ymax' => 60})
+
+exp = []
+run.times { exp <<  RandomFunctions.Uniform(0,5)}
+output.setArray(exp, "Uniform", {"histogram" => true, 'ymin' => 0, 'ymax' => 5 })
+
+gaussX = []
+gaussY = []
 puts "Gaussian"
-10.times {puts RandomFunctions.Gaussian(0, 1)}
+run.times do 
+	x,y = RandomFunctions.Gaussian(0, 1)
+	gaussX << x
+	gaussY << y
+end
+output.setXY([gaussX, gaussY], "Gauss")
+output.setArray(gaussX, "GaussX", {"histogram" => true})
+output.setArray(gaussY, "GaussY", {"histogram" => true})
+
 puts "Normal"
-10.times {puts RandomFunctions.Normal(0, 10)}
+normal = []
+run.times {normal << RandomFunctions.Normal(0, 10)}
+output.setArray(normal, "Normal", {"histogram" => true, 'ymin' => 0, 'ymax' => 10})
